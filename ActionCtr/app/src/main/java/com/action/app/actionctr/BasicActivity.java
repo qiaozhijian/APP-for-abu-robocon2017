fbe0c7591af74b522d6ed939e184b7beba3798eb
qq563902455 <563902455@qq.com>
2016-12-08 19:47:11 +0800

    start

---
+++ ActionCtr/app/src/main/java/com/action/app/actionctr/BasicActivity.java
@@ -0,0 +1,10 @@
+package com.action.app.actionctr;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 56390 on 2016/12/7.
+ */
+
+public class BasicActivity extends AppCompatActivity {
+}
---
+++ ActionCtr/app/src/main/java/com/action/app/actionctr/BeginActivity.java
@@ -0,0 +1,23 @@
+package com.action.app.actionctr;
+
+import android.os.Bundle;
+
+public class BeginActivity extends BasicActivity {
+
+    // Used to load the 'native-lib' library on application startup.
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_begin);
+    }
+
+    /**
+     * A native method that is implemented by the 'native-lib' native library,
+     * which is packaged with this application.
+     */
+    public native String stringFromJNI();
+}

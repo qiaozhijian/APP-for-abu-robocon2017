bbfb627ae20eb6eb80d0d6e3babf2de1034fd9f3
tianchang <729376693@qq.com>
2017-01-13 02:27:02 +0800

    右滑菜单中增加点击 蓝牙发送数据 未测试

--- ActionCtr/app/src/main/java/com/action/app/actionctr/MenuRightFragment.java
+++ ActionCtr/app/src/main/java/com/action/app/actionctr/MenuRightFragment.java
@@ -12,12 +12,14 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import com.action.app.actionctr.ble.bleDataProcess;
+
 /**
  * Created by lenovo on 2017/1/12.
  */
 
 public class MenuRightFragment extends Fragment implements AdapterView.OnItemClickListener {
-
+    private bleDataProcess bleCtr;
     private ListView menuList;
     private ArrayAdapter<String> adapter;
     private String[] menuItems = { "1", "2" ,"3","4","5","6","7","8"};
@@ -35,6 +37,7 @@ public class MenuRightFragment extends Fragment implements AdapterView.OnItemCli
 
     @Override
         public void onAttach(Context context) {
+               bleCtr=new bleDataProcess(context);
               super.onAttach(context);
               adapter = new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, menuItems);
         }
@@ -45,7 +48,36 @@ public class MenuRightFragment extends Fragment implements AdapterView.OnItemCli
         switch(i)
         {
             case 0:
-                Log.d("111","111");
+                Log.d("ctrCmd","button1");
+                bleCtr.sendCmd(1);
+                break;
+            case 1:
+                Log.d("ctrCmd","button2");
+                bleCtr.sendCmd(2);
+                break;
+            case 2:
+                Log.d("ctrCmd","button3");
+                bleCtr.sendCmd(3);
+                break;
+            case 3:
+                Log.d("ctrCmd","button4");
+                bleCtr.sendCmd(4);
+                break;
+            case 4:
+                Log.d("ctrCmd","button5");
+                bleCtr.sendCmd(5);
+                break;
+            case 5:
+                Log.d("ctrCmd","button6");
+                bleCtr.sendCmd(6);
+                break;
+            case 6:
+                Log.d("ctrCmd","button7");
+                bleCtr.sendCmd(7);
+                break;
+            case 7:
+                Log.d("ctrCmd","button8");
+                bleCtr.sendCmd(8);
                 break;
 
         }
